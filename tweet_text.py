f7c424736cfb03ef52a02e63cf7ae5ca8a2f2f7a
Zeiske <Milan.Zeiske@hpi.uni-potsdam.de>
2018-05-15 18:57:20 +0200

    zufällige Zufälle

--- tweet_text.py
+++ tweet_text.py
@@ -5,6 +5,8 @@ from datetime import datetime
 # convert times according to time zones
 from pytz import timezone
 
+import random
+
 def reply(tweet):
     """Return text to be used as a reply"""
     message = tweet['text']
@@ -19,6 +21,18 @@ def idle_text():
     """Return text that is tweeted when not replying"""
     # Construct the text we want to tweet out (280 chars max)
     berlin_time = datetime.now(timezone('Europe/Berlin'))
-    formatted_time = berlin_time.strftime("%H:%M:%S on a %A")
-    text = "It is " + formatted_time + "!"
+    formatted_time = berlin_time.strftime("%H:%M:%S")
+    random.seed(formatted_time)
+    text = "I have a meeting with my " + random.choice((
+		"doctor", 
+		"hairdresser", 
+		"manager", 
+		"dentist",
+		"friends",
+		"brother",
+		"sister",
+		"grandparents",
+		"teacher",
+		"classmate",
+		)) + " at " + formatted_time + "."
     return text
